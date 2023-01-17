c34c08fd6dbb659625acfa654ca8d4d7b1e00d0f
ssryu <jjkztok3@gmail.com>
2023-01-17 20:35:07 +0900

    Add webtoon listview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon_app/models/webtoon_model.dart';
+import 'package:webtoon_app/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +22,67 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Expanded(child: makeWebtoonList(snapshot)),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  ListView makeWebtoonList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemCount: snapshot.data!.length,
+      itemBuilder: ((context, index) {
+        final webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.3),
+                  ),
+                ],
+              ),
+              width: 250,
+              child: Image.network(webtoon.thumb),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(fontSize: 22),
+            ),
+          ],
+        );
+      }),
+      separatorBuilder: (context, index) {
+        return Container(
+          width: 40,
+        );
+      },
     );
   }
 }
