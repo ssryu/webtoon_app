14bcfcc92a4b2200ca519e25a9f032ab33dbf3f5
ssryu <jjkztok3@gmail.com>
2023-01-17 09:53:14 +0900

    Add homescreen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Today's Toons",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
