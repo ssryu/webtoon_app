3511ab16b333b95dfc64fb2acf457a6dc8f73c88
ssryu <jjkztok3@gmail.com>
2023-01-18 10:36:50 +0900

    Update webtoon detail screen

--- lib/screens/webtoon_detail_screen.dart
+++ lib/screens/webtoon_detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon_app/models/webtoon_detail_model.dart';
+import 'package:webtoon_app/models/webtoon_episode_model.dart';
+import 'package:webtoon_app/services/api_service.dart';
 
-class WebtoonDetailScreen extends StatelessWidget {
+class WebtoonDetailScreen extends StatefulWidget {
   final String title;
   final String thumb;
   final String id;
@@ -13,6 +16,21 @@ class WebtoonDetailScreen extends StatelessWidget {
   });
 
   @override
+  State<WebtoonDetailScreen> createState() => _WebtoonDetailScreenState();
+}
+
+class _WebtoonDetailScreenState extends State<WebtoonDetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatest10EpisodesById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -21,41 +39,122 @@ class WebtoonDetailScreen extends StatelessWidget {
         foregroundColor: Colors.green,
         backgroundColor: Colors.white,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 24,
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
             children: [
-              Hero(
-                tag: id,
-                child: Container(
-                  width: 350,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(10, 10),
-                        color: Colors.black.withOpacity(0.3),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 15,
+                            offset: const Offset(10, 10),
+                            color: Colors.black.withOpacity(0.3),
+                          ),
+                        ],
+                      ),
+                      child: Image.network(widget.thumb),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: webtoon,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Text(
+                        "${snapshot.data!.genre} / ${snapshot.data!.age}",
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
                       ),
                     ],
-                  ),
-                  child: Image.network(thumb),
-                ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: episodes,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
+                  // データが10個だけなので、リストビューを使わずにColumnで簡単に実装する。
+                  // データが多い場合はリストビューが望ましい。
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      for (final episode in snapshot.data!)
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.green.shade400,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 20,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  episode.title,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                const Icon(
+                                  Icons.chevron_right_rounded,
+                                  color: Colors.white,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                    ],
+                  );
+                },
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webtoon_app/models/webtoon_detail_model.dart';
+import 'package:webtoon_app/models/webtoon_episode_model.dart';
 import 'package:webtoon_app/models/webtoon_model.dart';
 
 class ApiService {
@@ -24,5 +26,33 @@ class ApiService {
     }
 
     return webtoonInstances;
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final uri = Uri.parse("$baseUrl/$id");
+
+    final response = await http.get(uri);
+    if (response.statusCode != 200) {
+      throw Error();
+    }
+
+    final Map<String, dynamic> webtoon = jsonDecode(response.body);
+    return WebtoonDetailModel.fromJson(webtoon);
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatest10EpisodesById(
+      String id) async {
+    final uri = Uri.parse("$baseUrl/$id/episodes");
+
+    final response = await http.get(uri);
+    if (response.statusCode != 200) {
+      throw Error();
+    }
+
+    final List<WebtoonEpisodeModel> episodes = [];
+    for (final episodeJson in jsonDecode(response.body)) {
+      episodes.add(WebtoonEpisodeModel.fromJson(episodeJson));
+    }
+    return episodes;
   }
 }
