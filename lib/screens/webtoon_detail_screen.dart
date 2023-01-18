c4fed34fb62a7fb4d9a2951df9719c382409b023
ssryu <jjkztok3@gmail.com>
2023-01-18 10:58:22 +0900

    Add link to comic.naver.com

--- lib/screens/webtoon_detail_screen.dart
+++ lib/screens/webtoon_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:webtoon_app/models/webtoon_detail_model.dart';
 import 'package:webtoon_app/models/webtoon_episode_model.dart';
 import 'package:webtoon_app/services/api_service.dart';
+import 'package:webtoon_app/widgets/episode_widget.dart';
 
 class WebtoonDetailScreen extends StatefulWidget {
   final String title;
@@ -119,34 +120,9 @@ class _WebtoonDetailScreenState extends State<WebtoonDetailScreen> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       for (final episode in snapshot.data!)
-                        Container(
-                          margin: const EdgeInsets.only(bottom: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.green.shade400,
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 10,
-                              horizontal: 20,
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  episode.title,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                const Icon(
-                                  Icons.chevron_right_rounded,
-                                  color: Colors.white,
-                                )
-                              ],
-                            ),
-                          ),
+                        Episode(
+                          episode: episode,
+                          webtoonId: widget.id,
                         ),
                     ],
                   );
